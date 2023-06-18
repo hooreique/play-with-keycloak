771ae5e7149397f1031fb86dafd83e3bacd5db71
Je-yeong Song <46372718+hooreique@users.noreply.github.com>
2023-06-18 18:03:37 +0900

    chore: Update pwsh scripts

--- run-my-keycloak.ps1
+++ run-my-keycloak.ps1
@@ -1,6 +1,19 @@
-docker run `
-    -p 8333:8080 `
-    -e KEYCLOAK_ADMIN=sa `
-    -e KEYCLOAK_ADMIN_PASSWORD="A0123456" `
-    --name my-keycloak `
-    quay.io/keycloak/keycloak:21.1.1 start-dev
+if (
+    docker ps -f "name=my-keycloak" --format "{{.Names}}" ` 
+        | Select-String -Pattern "^my-keycloak$" -Quiet
+) {
+    Write-Host "Container my-keycloak is currently running"
+} elseif (
+    docker ps -a -f "name=my-keycloak" --format "{{.Names}}" ` 
+        | Select-String -Pattern "^my-keycloak$" -Quiet
+) {
+    docker start my-keycloak
+} else {
+    docker run `
+        -d `
+        -p 8333:8080 `
+        -e KEYCLOAK_ADMIN=sa `
+        -e KEYCLOAK_ADMIN_PASSWORD="A0123456" `
+        --name my-keycloak `
+        quay.io/keycloak/keycloak:21.1.1 start-dev
+}
--- stop-my-keycloak.ps1
+++ stop-my-keycloak.ps1
@@ -1,5 +1,5 @@
 if (docker ps -q -f "name=my-keycloak") {
     docker stop my-keycloak
 } else {
-    Write-Host "Container named my-keycloak is not currently running"
+    Write-Host "Container my-keycloak is not currently running"
 }
